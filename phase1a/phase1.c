e890f00424c5ce7da42a4171c01fd778cc5764c0
kvncn <kevin@nisterenko.com>
2023-01-31 09:00:34 -0700

    dumb shit

--- phase1a/phase1.c
+++ phase1a/phase1.c
@@ -436,7 +436,7 @@ int testcase_mainProc(char* usloss) {
  */
 void kernelCheck(char* proc) {
     // means we are running in user mode, so we halt simulation
-    if ((USLOSS_PsrGet() & USLOSS_PSR_CURRENT_MODE) == 1) {
+    if ((USLOSS_PsrGet() & USLOSS_PSR_CURRENT_MODE) == 0) {
         USLOSS_Console("ERROR: Someone attempted to call %s while in user mode!\n", proc);
         USLOSS_Halt(1);
     }
