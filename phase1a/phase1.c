7301113a436dde58d8b06963c722ba106c240e91
kvncn <kevin@nisterenko.com>
2023-01-31 19:33:19 -0700

    Comments finished, phase1a DONE!

--- phase1a/phase1.c
+++ phase1a/phase1.c
@@ -97,10 +97,13 @@ int procCount;                 // how many process currently in the table
 
 /**
  * Bootstarp for the process table and the init process, only populates 
- * the process table with 
+ * the process table with empty processes, so that we can actually init them 
+ * once we run them. 
  */
 void phase1_init(void) {
     kernelCheck("phase1_init");
+
+    // initializing the process table (global data structure)
     for (int i = 0; i < MAXPROC; i++) {
         cleanEntry(i); 
     }
@@ -110,6 +113,7 @@ void phase1_init(void) {
 
     int slot = slotFinder();
     
+    // fill in the information for setup of init process
     strcpy(ProcessTable[slot].name, "init");
     ProcessTable[slot].PID = pidIncrementer;
     ProcessTable[slot].args[0] = '\0';
@@ -124,6 +128,7 @@ void phase1_init(void) {
     USLOSS_ContextInit(&ProcessTable[slot].context, ProcessTable[slot].stack, 
                        ProcessTable[slot].stSize, NULL, trampoline);
 
+    // make the init the current process so we can later run it
     CurrProcess = &ProcessTable[slot];
 
     pidIncrementer++;
@@ -132,22 +137,35 @@ void phase1_init(void) {
 }
 
 /**
- * 
+ * This function starts the running of processes, changes init from runnable to
+ * running. Uses one context switch.
  */
 void startProcesses(void) {
-
-    // do we disable interrupts and enable them here?
-
     CurrProcess->state = RUNNING;
     USLOSS_ContextSwitch(NULL, &CurrProcess->context);
 }
- 
+
+/**
+ * This function simulates a fork call to create children processes based on the
+ * parent process (currProcess), it doesn't do anything like duplicating it, but 
+ * does create a children process on the table if possible and sets it to running.
+ * 
+ * @param name, char pointer, name of the new process
+ * @param func, function representing the main of the process
+ * @param arg, char pointer representing the arguments to be passed to the
+ * process' main
+ * @param stacksize, integer representing the size of the stack allocated for
+ * this process
+ * @param priority, integer representing the priority of the child process
+ * @return process id of the newly created process, or an error code if there 
+ * was an error when creating the process
+ */
 int fork1(char *name, int(*func)(char *), char *arg, int stacksize, int priority) {
     kernelCheck("fork1");	
 
     disableInterrupts();
 
-    // problem with args or num processes
+    // problem with args or num processes, maybe out of priority
     if (name == NULL || strlen(name) > MAXNAME || func == NULL || 
         priority >= LOW_PRIORITY-1 || priority < HIGH_PRIORITY || 
         procCount == MAXPROC) {
@@ -159,21 +177,28 @@ int fork1(char *name, int(*func)(char *), char *arg, int stacksize, int priority
         return -2;
     }
 
+    // increase the number of processes
     procCount++;
 
+    // get the next slot depending on the pid 
     int slot = slotFinder();
+
+    // if we had an invalid slot, we return and don't fork 
     if (slot == -1) {
         return -1;
     }
 
     strcpy(ProcessTable[slot].name, name);
 
+    // if the arguments for the process were NULL we set it to null char,
+    // otherwsie, copy the string
     if (arg == NULL) {
         ProcessTable[slot].args[0] = '\0';
     } else {
         strcpy(ProcessTable[slot].args, arg);
     }
 
+    // fill in process info
     ProcessTable[slot].PID = pidIncrementer;
     ProcessTable[slot].processMain = func;
     ProcessTable[slot].stSize = stacksize;
@@ -185,23 +210,20 @@ int fork1(char *name, int(*func)(char *), char *arg, int stacksize, int priority
 
     pidIncrementer++;
 
+    // if this is the first child just set it, otherwise we can just get the
+    // child reference and update the list pointer
     if (CurrProcess->firstChild == NULL) {
         CurrProcess->firstChild = &ProcessTable[slot];
-        
     } else {
         // adding to head
         Process* cur = CurrProcess->firstChild;
 
         ProcessTable[slot].firstSibling = cur;
         CurrProcess->firstChild = &ProcessTable[slot];
-
-        // while (cur->firstSibling != NULL) {
-        //     cur = cur->firstSibling;
-        // }
-        // cur->firstSibling = &ProcessTable[slot];
     }
     CurrProcess->numChildren++;
 
+    // initialize the context and then restore interrupts
     USLOSS_ContextInit(&ProcessTable[slot].context, ProcessTable[slot].stack, 
                        ProcessTable[slot].stSize, NULL, &trampoline);
 
@@ -210,32 +232,40 @@ int fork1(char *name, int(*func)(char *), char *arg, int stacksize, int priority
     return ProcessTable[slot].PID;
 }
 
+/**
+ * This function alerts the parent process about the dead children it finds,
+ * it gives information about the exit state and also the process id of the 
+ * removed process. Also performs clean up of the Process Table after 
+ * dead process removal.
+ * 
+ * @param status, integer pointer representing the status of the exited 
+ * process, we set it after we find the dead child
+ * @return integer representing the process id of the dead child that join
+ * found
+ */
 int join(int *status) {
     kernelCheck("join");
 
     disableInterrupts();
 
-    // do we even have children??
+    // check if we have any children
     if (CurrProcess->numChildren == 0) {
-        //USLOSS_Console("No children, join fails\n");
-        // USLOSS_Console("CURR PID %s\n", CurrProcess->name);
-        // USLOSS_Console("NO CHILDREN\n");
-        // USLOSS_Halt(4);
         return -2;
     }
 
-    // USLOSS_Console("Checked for children join\n");
     // remove dead child
     Process* child = CurrProcess->firstChild;
 
     Process* removed = NULL;
 
-    // if dead is head
+    // if head is the dead child, we just remove it, otherwise, find the
+    // first dead child
     if (child->state == DEAD) {
         removed = child;
         CurrProcess->firstChild = child->firstSibling;
     // remove from rest, first dead child we find
     } else {
+        // iterate over the list, until we find a dead child, remove it
         while (child->firstSibling->state != DEAD) {
             child = child->firstSibling;
         }
@@ -244,12 +274,12 @@ int join(int *status) {
     }
 
 
-    //no one dead
+    // if no one dead, just halt the sim, quit join should find a dead child
     if (removed == NULL) {
-        //USLOSS_Console("NO DEAD CHILDREN\n");
         USLOSS_Halt(4);
     }
 
+    // clean up the dead children after reading its exit status
     CurrProcess->numChildren--;
 
     *status = removed->exitState;
@@ -264,16 +294,21 @@ int join(int *status) {
     return res;
 }
 
-// might be needing a lot of work ngl
+/**
+ * Terminates the execution of the current process, saves the exit status 
+ * passed into it and switches out of it. 
+ * 
+ * @param status, integer representing exit status of the process after we 
+ * kill it
+ * @param switchToPid, integer representing the pid of the next process to 
+ * run since we don't yet have a dispatcher
+ */
 void quit(int status, int switchToPid) {
     kernelCheck("quit");
 
     // never returns, goes inside other funcs, so 
-    // no need to re enable
+    // no need to restore interrupts, leave this to the trampoline
     disableInterrupts();
-
-    // i am not runnable, possible rc, if a contextswitch happens
-    // disable interrupts to deal with them
 
     // if parent dies before all children, halt sim
     if (CurrProcess->numChildren > 0) {
@@ -281,24 +316,17 @@ void quit(int status, int switchToPid) {
         USLOSS_Halt(3);
     }
 
-    // if this is the first child, make the next child the first sibling
-    // if (CurrProcess->parent->firstChild->PID == CurrProcess->PID) {
-    //     CurrProcess->parent->firstChild = CurrProcess->firstSibling;
-    // }
-
+    // make this process dead and store exit status
     CurrProcess->state = DEAD;
     CurrProcess->exitState = status;
-
-    // just clean the entry since we have no parents to report to
-    // if (CurrProcess->parent == NULL) {
-    //     cleanEntry(CurrProcess->slot);
-    // }
 
     // since we quit, decrease count
     procCount--;
     
-    // quit doesn't care about saving prev context
+    // quit doesn't care about saving prev context, so we just 
+    // get the next PID from the param, and call a context switch
     for (int i = 0; i < MAXPROC; i++) {
+        // found PID in table, we are assuming it exists
         if (ProcessTable[i].PID == switchToPid) {
             CurrProcess = &ProcessTable[i];
             break;
@@ -315,6 +343,8 @@ void quit(int status, int switchToPid) {
 void dumpProcesses(void) {
     USLOSS_Console(" PID  PPID  NAME              PRIORITY  STATE\n");
 
+    // for every process, we want to print a human readable version of the
+    // state
     for (int i=0; i < MAXPROC; i++) {
         Process *slot = &ProcessTable[i];
         if (slot->PID == 0)
@@ -332,6 +362,11 @@ void dumpProcesses(void) {
     }
 }
 
+/**
+ * Returns current process' id.
+ * 
+ * @return integer representing the process id of the current process
+ */
 int getpid() {
     kernelCheck("getpid");
     disableInterrupts();
@@ -361,8 +396,16 @@ void TEMP_switchTo(int newpid) {
 
 // ----- Special Processes
 
-// init
-
+/**
+ * init process serves to bootstrap and kickstart our simulation, it runs 
+ * at the second lowest priority and calls the service processes from 
+ * other phases. Also here is where we create sentinel and testcase_main, also
+ * context switch into testcase_main. We include an error checking join loop 
+ * here as well. 
+ * 
+ * @param usloss, char pointer representing the arguments of this process' main
+ * @return integer representing this process' main function return
+ */
 int init(char* usloss) {
     // calling the service funcs for different phases
     phase2_start_service_processes();
@@ -370,13 +413,11 @@ int init(char* usloss) {
 	phase4_start_service_processes();
 	phase5_start_service_processes();
     
-    // creating sentinel
+    // creating sentinel manually since it is the lowest priority and thus
+    // disallowed by fork, better than changing our fork implementation
     procCount++;
-
     CurrProcess->state = RUNNABLE;
-
     int slot = slotFinder();
-
     strcpy(ProcessTable[slot].name, "sentinel");
     
     ProcessTable[slot].args[0] = '\0';
@@ -392,12 +433,14 @@ int init(char* usloss) {
     CurrProcess->numChildren++;
     CurrProcess->firstChild = &ProcessTable[slot];
 
+    // still need to init the context even though we don't expect it to run
     USLOSS_ContextInit(&ProcessTable[slot].context, ProcessTable[slot].stack, 
                        ProcessTable[slot].stSize, NULL, &trampoline);
     
     pidIncrementer++;
     
     USLOSS_Console("Phase 1B TEMPORARY HACK: init() manually switching to testcase_main() after using fork1() to create it.\n");
+
     // calling fork for testcase_main
     fork1("testcase_main", &testcase_mainProc, NULL, USLOSS_MIN_STACK, 3);
 
@@ -406,22 +449,29 @@ int init(char* usloss) {
 
     CurrProcess->state = RUNNING;
 
+    // start running testcase_main 
     USLOSS_ContextSwitch(&old->context, &CurrProcess->context);
 
     int res; 
-    
+    // here we have a while true loop to check for possible errors on join 
+    // stemming from this process
     while (1) {
         res = join(&res);
         if (res == -2) 
             USLOSS_Console("ERROR: init() received -2 in infinite join() loop");
             break;
     }
-    
     return 0;
-    
 }
 
-// sentinel 
+/**
+ * The sentinel process serves as a deadlock check and it should only run if
+ * nothing else can, ie, it is the lowest priority process. This is Russ' 
+ * implementation from the spec.
+ * 
+ * @param usloss, char pointer representing the arguments of this process' main
+ * @return integer representing this process' main function return
+ */
 int sentinel(char* usloss) {
     while (1) {
         if (phase2_check_io() == 0)
@@ -432,8 +482,15 @@ int sentinel(char* usloss) {
     return 0;
 }
 
-// testcase_main
-
+/**
+ * This function serves as a way to call the testcase's main function. If it 
+ * ever returns, we output the error, however, on phase1a this is normal 
+ * termination and expected, so we exit with normal termination of zero and
+ * halt the simulation.
+ * 
+ * @param usloss, char pointer representing the arguments of this process' main
+ * @return integer representing this process' main function return
+ */
 int testcase_mainProc(char* usloss) {
     int res = testcase_main();
     
@@ -450,9 +507,11 @@ int testcase_mainProc(char* usloss) {
 /**
  * Checks whether or not the simulation is running in kernel mode and halts the
  * entire simulation if so. 
+ * 
+ * @param proc, char pointer representing the process' name so we can output 
+ * a good error message as to what process tried running in user mode
  */
 void kernelCheck(char* proc) {
-    //USLOSS_Console("CURRENT STUFF %d\n", USLOSS_PsrGet());
     // means we are running in user mode, so we halt simulation
     if ((USLOSS_PsrGet() & USLOSS_PSR_CURRENT_MODE) == 0) {
 		USLOSS_Console("ERROR: Someone attempted to call %s while in user mode!\n", proc);
@@ -462,18 +521,19 @@ void kernelCheck(char* proc) {
 
 /**
  * This is used so that we can actually correctly call and never return from 
- * a process' main. 
+ * a process' main. It is a wrapper for USLOSS.
  */
 void trampoline() {
     restoreInterrupts();
     CurrProcess->state = RUNNING;
     // call the process' main func, with its own args
     int res = CurrProcess->processMain(CurrProcess->args);
-    // quit on it, 1a will never return? so do we need to do it??? prob not for
-    // 1a
-
-    USLOSS_Halt(2);
     
+    // we don;t ever want to return, so if we actually get here we halt the
+    // simulation with an error
+    USLOSS_Halt(2);
+
+    // exit the current process
     quit(res, CurrProcess->parent->PID);
 }
 
@@ -493,7 +553,10 @@ void restoreInterrupts() {
 
 /**
  * Cleans a specific entry in the process table so we can 
- * initialize it/quit process
+ * initialize it/quit the process. 
+ * 
+ * @param idx, index of the process we want to clean up in the
+ * process table
  */
 void cleanEntry(int idx) {
     ProcessTable[idx].name[0] = '\0';
